9cf55b9dfd77450351686d425edb81a5f48d027f
Nick Shockey <shockeynj@gmail.com>
2023-02-01 16:26:27 -0500

    updated configs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,43 @@
 module.exports = {
 	root: true,
-	parser: '@typescript-eslint/parser',
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-	plugins: ['svelte3', '@typescript-eslint'],
-	ignorePatterns: ['*.cjs'],
-	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
-	settings: {
-		'svelte3/typescript': () => require('typescript')
-	},
-	parserOptions: {
-		sourceType: 'module',
-		ecmaVersion: 2020
-	},
 	env: {
 		browser: true,
 		es2017: true,
-		node: true
-	}
+		node: true,
+	},
+	extends: [
+		"eslint:recommended",
+		"plugin:@typescript-eslint/recommended",
+		"prettier",
+	],
+	overrides: [{ files: ["*.svelte"], processor: "svelte3/svelte3" }],
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		ecmaVersion: latest,
+		sourceType: "module",
+	},
+	plugins: ["svelte3", "@typescript-eslint"],
+	ignorePatterns: ["*.cjs"],
+	settings: {
+		"svelte3/typescript": () => require("typescript"),
+	},
+	rules: {
+		"@typescript-eslint/naming-convention": [
+			"warn",
+			{
+				selector: "variable",
+				format: ["camelCase"],
+			},
+			{
+				selector: "class",
+				format: ["PascalCase"],
+			},
+		],
+		"curly": "error",
+		"eqeqeq": "error",
+		"no-throw-literal": "warn",
+		"prefer-const": "warn",
+		"semi": ["error", "always"],
+		"quotes": ["error", "double"],
+	},
 };
