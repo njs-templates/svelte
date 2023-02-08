2139d951dffa0936263e5e3d0ddadac5eaa9e1ca
Nick Shockey <shockeynj@gmail.com>
2023-02-08 14:56:38 -0500

    Edited pages to be more like other templates
    Pics don't work yet

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,52 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-	Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+<script lang="ts">
+	import HelloWorld from "$lib/components/HelloWorld.svelte";
+</script>
 
-<button class="btn btn-primary">Wow</button>
+<main>
+	<div class="main-content">
+		<a
+			href="https://vitejs.dev"
+			target="_blank"
+			rel="noreferrer"
+		>
+			<img
+				src="/vite.svg"
+				class="logo"
+				alt="Vite logo"
+			/>
+		</a>
+		<a
+			href="https://vuejs.org/"
+			target="_blank"
+			rel="noreferrer"
+		>
+			<img
+				src="../assets/vue.svg"
+				class="logo vue"
+				alt="Svelte logo"
+			/>
+		</a>
+	</div>
+	<HelloWorld msg="Vite + Svelte Kit + TypeScript" />
+</main>
+
+<style scoped>
+	.main-content {
+		@apply flex place-content-center;
+		margin: 0 auto;
+	}
+
+	.logo {
+		height: 10em;
+		padding: 1.5em;
+		will-change: filter;
+	}
+
+	.logo:hover {
+		filter: drop-shadow(0 0 2em #646cffaa);
+	}
+
+	.logo.vue:hover {
+		filter: drop-shadow(0 0 2em #42b883aa);
+	}
+</style>
