d36eecf040d65007ea2229a84cc2368da6d958e0
Nick Shockey <shockeynj@gmail.com>
2023-02-08 14:13:08 -0500

    NavBar now updates on page load

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,11 @@
 <script lang="ts">
-	import "../app.css";
+	import { onMount } from "svelte";
+	import { setPath } from "$lib/modules/getPath";
 	import NavBar from "$lib/components/NavBar.svelte";
+	import "../app.css";
+
+	// Update navbar on load
+	onMount(setPath);
 </script>
 
 <main>
